381014a8d2ebc16cd12fc375e52c97130a626652
wpt.osp@gmail.com <wpt.osp@gmail.com@53a96681-6e17-ebfa-daa9-5dccc4ee52eb>
2015-03-23 17:46:58 +0000

    Added ToString() method

--- trunk/PTIC/PTIC.Sale/Entities/ContactPerson.cs
+++ trunk/PTIC/PTIC.Sale/Entities/ContactPerson.cs
@@ -33,5 +33,30 @@ namespace PTIC.Sale.Entities
         public DateTime LastModified { get; set; }
         public bool IsDeleted { get; set; }
         #endregion
+
+        override public string ToString()
+        {
+            string str = String.Empty;
+            str = String.Concat(str, "ID = ", ID, "\r\n");
+            str = String.Concat(str, "AddrID = ", AddrID, "\r\n");
+            str = String.Concat(str, "CusID = ", CusID, "\r\n");
+            str = String.Concat(str, "ContactPersonName = ", ContactPersonName, "\r\n");
+            str = String.Concat(str, "DOB = ", DOB, "\r\n");
+            str = String.Concat(str, "NRC = ", NRC, "\r\n");
+            str = String.Concat(str, "Post = ", Post, "\r\n");
+            str = String.Concat(str, "Email = ", Email, "\r\n");
+            str = String.Concat(str, "MobilePhone = ", MobilePhone, "\r\n");
+            str = String.Concat(str, "HomePhone = ", HomePhone, "\r\n");
+            str = String.Concat(str, "Membership = ", Membership, "\r\n");
+            str = String.Concat(str, "Education = ", Education, "\r\n");
+            str = String.Concat(str, "SpouseName = ", SpouseName, "\r\n");
+            str = String.Concat(str, "Race = ", Race, "\r\n");
+            str = String.Concat(str, "Religion = ", Religion, "\r\n");
+            str = String.Concat(str, "Remark = ", Remark, "\r\n");
+            str = String.Concat(str, "DateAdded = ", DateAdded, "\r\n");
+            str = String.Concat(str, "LastModified = ", LastModified, "\r\n");
+            str = String.Concat(str, "IsDeleted = ", IsDeleted, "\r\n");
+            return str;
+        }
     }
 }
